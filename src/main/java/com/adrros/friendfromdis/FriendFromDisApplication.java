4beca3b5212f67d9a24c4c2972cf25d0aafc98a3
NPExce <adrian.rostkowski01@gmail.com>
2024-11-30 10:18:09 +0100

    first commit

---
+++ src/main/java/com/adrros/friendfromdis/FriendFromDisApplication.java
@@ -0,0 +1,13 @@
+package com.adrros.friendfromdis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendFromDisApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(FriendFromDisApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/adrros/friendfromdis/FriendFromDisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrros.friendfromdis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendFromDisApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
